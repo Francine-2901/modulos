38b2229be03034d804b1f023378b0e27395862dc
Francine-2901 <francinefacul07@gmail.com>
2022-06-30 22:03:43 -0300

    modulos

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade('Namjoon', 27));
+console.log(mostraCidade('Namjoon', 'Seul'));
+console.log(mostraHobby('Namjoon', 'Visitar museu'));
